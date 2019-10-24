3c1b0b26925f8453ca1490a8f1a878511101eb8b
mmustafau <mmustafau@gmail.com>
2019-10-24 17:56:37 +0300

    seconde rest api   hateaoas

--- springresto-master/src/main/java/com/mustafa/springresto/SpringrestoApplication.java
+++ springresto-master/src/main/java/com/mustafa/springresto/SpringrestoApplication.java
@@ -1,7 +1,11 @@
 package com.mustafa.springresto;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
 
 @SpringBootApplication
 public class SpringrestoApplication {
@@ -13,7 +17,32 @@ public class SpringrestoApplication {
 
         System.out.println("asdasdasd");
 
+
         System.out.println("asdasdasd");
+
+        Thread thread =new Thread(new Runnable() {
+
+            int asd=0;
+
+            @Override
+            public void run() {
+            while(true){
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(asd);
+                asd++;
+                asd++;
+
+            }}
+        });
+
+        thread.start();
+
 
     }
 
